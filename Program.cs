d3a1dcbc87d1b349ed5a5c245a7a803a44f9f659
Bipeen <acharyab@seas.upenn.edu>
2019-04-12 12:47:28 -0700

    Initial check-in of module ConsoleTeleprompter

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace TeleprompterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = ReadFrom("sampleQuotes.txt");
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine("Hello World!");
+        }
+
+        static IEnumerable<string> ReadFrom(string file)
+        {
+            string line;
+            using (var reader = File.OpenText(file))
+            {
+                while ((line = reader.ReadLine()) != null )
+                {
+                    yield return line;
+                }
+            }
+        }
+    }
+}
